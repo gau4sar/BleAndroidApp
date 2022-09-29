0fba20f2c0d7bcfea7bf69e6592f65055ed1688e
Gaurav Sarma <gaurav4sarma@gmail.com>
2022-09-29 15:24:38 +0530

    Permissions is added to manifest for BLUETOOTH

--- app/src/main/java/io/ballerine/kmp/bleandroidapp/MainActivity.kt
+++ app/src/main/java/io/ballerine/kmp/bleandroidapp/MainActivity.kt
@@ -1,41 +1,193 @@
 package io.ballerine.kmp.bleandroidapp
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.ballerine.kmp.bleandroidapp.ui.theme.BleAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String? = intent.action
+            when (action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+
+                    if (ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        val deviceName = device?.name
+                        val deviceHardwareAddress = device?.address // MAC address
+
+                        Log.d("nearby", "deviceName $deviceName")
+                    }
+                }
+            }
+        }
+    }
+
+    var discoverableLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+                Log.d("discoverableLauncher", "PERMISSION GRANTED")
+            } else {
+
+                Log.d("discoverableLauncher", "PERMISSION not granted")
+            }
+        }
+
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BleAndroidAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    BluetoothDevicesListScreen(showPairedDevices = {
+                        showPairedDevices()
+                    })
                 }
             }
         }
+
+        // Register for broadcasts when a device is discovered.
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+
+        showPairedDevices()
+    }
+
+    private fun showPairedDevices() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        } else {
+
+            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+            pairedDevices?.forEach { device ->
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+
+                Log.d("pairedDevices", "deviceName $deviceName")
+            }
+
+            /*val discoverableIntent: Intent =
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                    putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+                }
+
+            // Asking for permission
+            discoverableLauncher.launch(discoverableIntent)*/
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver)
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BluetoothDevicesListScreen(showPairedDevices: () -> Unit) {
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission Accepted: Do something
+            Log.d("ExampleScreen", "PERMISSION GRANTED")
+
+        } else {
+            // Permission Denied: Do something
+            Log.d("ExampleScreen", "PERMISSION DENIED")
+        }
+    }
+
+    val context = LocalContext.current
+
+    Button(onClick = {
+        // Check permission
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) -> {
+                // Some works that require permission
+                Log.d("ExampleScreen", "Code requires permission")
+                showPairedDevices()
+            }
+            else -> {
+                // Asking for permission
+                launcher.launch(Manifest.permission.BLUETOOTH_SCAN)
+            }
+        }
+    }) {
+        Text(text = "Show Paired Devices")
+    }
 }
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BleAndroidAppTheme {
-        Greeting("Android")
+        BluetoothDevicesListScreen(showPairedDevices = {})
     }
 }
